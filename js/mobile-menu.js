95bc5cce5e2e1caeb877839d9a7904a2070b1605
Kateryna Vasiuk <catherine.vasyuk11@gmail.com>
2022-11-19 00:09:16 +0200

    edit js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,7 +4,7 @@
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
     body: document.querySelector('body'),
-    menuList: document.querySelector('.menu__mobile__list'),
+    menuList: document.querySelector('.header-menu-list'),
   };
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
