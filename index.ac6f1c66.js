c4bec7d0c0ffb5a45d3bd22009309b555fd387a6
KHARKIVSKA-KURKUMA <99280393+KHARKIVSKA-KURKUMA@users.noreply.github.com>
2023-04-10 19:16:30 +0000

    Deploying to gh-pages from @ KHARKIVSKA-KURKUMA/Vantage-Studio@4194158d26b1311e13031dcd03df068dadf4b01d 🚀

---
+++ index.ac6f1c66.js
@@ -0,0 +1,2 @@
+var upBtn=document.getElementById("up");function scrollFunction(){document.body.scrollTop>400||document.documentElement.scrollTop>400?(upBtn.style.display="flex",upBtn.classList.remove("is-hidden")):upBtn.style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=function(){scrollFunction()},upBtn.addEventListener("click",topFunction);
+//# sourceMappingURL=index.ac6f1c66.js.map
